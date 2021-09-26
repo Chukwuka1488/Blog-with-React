38e6e1d0a52c724521d2b647e5817e7a3d26da41
Chuks Akibot <juniorakibor@yahoo.com>
2021-09-26 16:59:04 +0200

    dynamic values in template

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,11 +1,20 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const title = 'Welcome to the new blog';
+  const likes = 50;
+  const link = 'https://www.google.com';
+  //
   return (
     <div className='App'>
       <div className='content'>
-        <h1>App Component</h1>
+        <h1>{title}</h1>
+        <p>{likes}</p>
+        <p>{'Hello, React'}</p>
+        <p>{[1, 2, 3]}</p>
+        <p>{Math.random() * 10}</p>
+        <a href={link}>Google site</a>
       </div>
     </div>
   );
