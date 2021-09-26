95449353a8be076e0a48158baae84bc11c8f30a4
Chuks Akibot <juniorakibor@yahoo.com>
2021-09-26 20:22:39 +0200

    using props for reusable components

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
+import BlogList from './BlogList';
 
 const Home = () => {
-  //   let name = 'Mario';
   const [blogs, setBlogs] = useState([
     {
       title: 'My New Website',
@@ -22,16 +22,9 @@ const Home = () => {
       id: 3,
     },
   ]);
-
- 
   return (
     <div className='home'>
-      {blogs.map((blog) => (
-        <div className='blog-preview' key={blog.id}>
-          <h2>{blog.title}</h2>
-          <p>Written by {blog.author}</p>
-        </div>
-      ))}
+      <BlogList blogs={blogs} title="All blogs"></BlogList>
     </div>
   );
 };
